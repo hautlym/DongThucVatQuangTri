0085e5a2300b978b8b182244531b1b635438fc9a
Nguyễn Hậu <nguyenhautlym@gmail.com>
2024-02-27 21:29:36 +0700

    update feature send email

--- Areas/Admin/Controllers/LoginController.cs
+++ Areas/Admin/Controllers/LoginController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Net.Mail;
 using System.Security.Claims;
 using System.Text;
 using static Org.BouncyCastle.Crypto.Engines.SM2Engine;
@@ -124,10 +125,22 @@ namespace DongThucVatQuangTri.Areas.Admin.Controllers
                 ViewBag.ErrorForgotPass = "người dùng không tồn tại vui lòng liên hệ người quản lí";
                 return View();
             }
+
+            var img = $"{Request.Scheme}://{Request.Host.Value}"+ "/images/logo.png"; 
+            
             var token = await _userManager.GeneratePasswordResetTokenAsync(user);
             token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
             var confirmationLink = Url.Action("ResetPassword", "Login", new { userId = user.Id, code = token, Email = email }, Request.Scheme);
-            await _emailSender.SendEmailAsync(email, "Quên mật khẩu ", $"Để đặt lại mật khẩu hãy <a href='{confirmationLink}'>Bấm vào đây</a>.");
+            string temapletHTML =
+                $"\r\n<!doctype html>\r\n<html lang=\"en-US\">\r\n\r\n<head>\r\n    <meta content=\"text/html; charset=utf-8\" http-equiv=\"Content-Type\" />\r\n    <title>Quên mật khẩu</title>\r\n    <meta name=\"description\" content=\"Reset Password Email Template.\">\r\n    <style type=\"text/css\">\r\n        a:hover {{text-decoration: underline !important;}}\r\n    </style>\r\n</head>\r\n\r\n<body marginheight=\"0\" topmargin=\"0\" marginwidth=\"0\" style=\"margin: 0px; background-color: #f2f3f8;\" leftmargin=\"0\">\r\n    <!--100% body table-->\r\n    <table cellspacing=\"0\" border=\"0\" cellpadding=\"0\" width=\"100%\" bgcolor=\"#f2f3f8\"\r\n        " +
+                $"style=\"@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;\">\r\n        <tr>\r\n            <td>\r\n                <table style=\"background-color: #f2f3f8; max-width:670px;  margin:0 auto;\" width=\"100%\" border=\"0\"\r\n                    align=\"center\" cellpadding=\"0\" cellspacing=\"0\">\r\n                    <tr>\r\n                        <td style=\"height:10px;\">&nbsp;</td>\r\n                    </tr>\r\n                    <tr>\r\n                        <td style=\"text-align:center; font-weight: bold;font-size: 17px;\">\r\n                            " +
+                $"<img src=\"{img}\" style=\"height: 70px; width: 70px;\" />\r\n                            <div style=\"margin-top:5px ;\">CỤC LÂM NGHIỆP</div>\r\n                            <div style=\"margin-top:5px ;\">BỘ NÔNG NGHIỆP VÀ PHÁT TRIỂN NÔNG THÔN</div>\r\n                        </td>\r\n                    </tr>\r\n                    <tr>\r\n                        <td style=\"height:20px;\">&nbsp;</td>\r\n                    </tr>\r\n                    <tr>\r\n                        <td>\r\n                            <table width=\"95%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\"\r\n                                style=\"max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);" +
+                $"-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);\">\r\n                                <tr>\r\n                                    <td style=\"height:40px;\">&nbsp;</td>\r\n                                </tr>\r\n                                <tr>\r\n                                    <td style=\"padding:0 35px;\">\r\n                                        <h1 style=\"color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;\">Bạn có yêu cầu đặt lại mật khẩu</h1>\r\n                                        <span\r\n                                            style=\"display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;\"></span>\r\n                    " +
+                $"                    <p style=\"color:#455056; font-size:15px;line-height:24px; margin:0;\">\r\n                                            Chúng tôi không thể chỉ gửi cho bạn mật khẩu cũ của bạn. Một liên kết duy nhất để đặt lại mật khẩu đã được tạo cho bạn. Để đặt lại mật khẩu của bạn, hãy nhấp vào liên kết sau và làm theo hướng dẫn\r\n                                        </p>\r\n                                        <a href=\"{confirmationLink}\"\r\n                                            style=\"background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;\">Đặt lại mật khẩu</a>\r\n                                    </td>\r\n        " +
+                $"       </tr>\r\n  <tr>\r\n   <td style=\"height:40px;\">&nbsp;</td>\r\n         </tr>\r\n                            </table>\r\n                        </td>\r\n                     </table>\r\n            </td>\r\n    <tr>\r\n                        <td style=\"height:20px;\">&nbsp;</td>\r\n                    </tr>\r\n                         </tr>\r\n    </table>\r\n    <!--/100% body table-->\r\n</body>\r\n\r\n</html>";
+
+
+            await _emailSender.SendEmailAsync(email, "Quên mật khẩu ", temapletHTML);
             return RedirectToAction("NoticeEmail");
         }
         [HttpGet]
